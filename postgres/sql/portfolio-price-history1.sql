5d9be9a7eee8a35e925bbfe0e12ac29fd360c3e5
Sam <sammatthewsnz@gmail.com@gmail.com>
2018-11-27 22:30:12 +1300

    Added sql scripts to report on portfolio distribution information.

---
+++ postgres/sql/portfolio-price-history1.sql
@@ -0,0 +1,13 @@
+SELECT
+  p_date,
+  p_portfolio,
+  p_service,
+  p_fund,
+  ROUND(CAST(p_sma AS NUMERIC),4) AS SMA,
+  p_score,
+  ROUND(CAST(p_total AS NUMERIC),4) AS TOTAL
+FROM portfolio_price_history
+WHERE 1=1
+  AND p_portfolio IN ('TRUST','SAM','KIWI-SAM','KATE')
+ORDER BY p_date, p_portfolio, p_fund
+;
