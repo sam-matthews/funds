785f4bee0217511e2218e4072925b7ff9c639fd0
Sam <sammatthewsnz@gmail.com@gmail.com>
2019-02-24 10:12:55 +1300

    Implement changes to manage and calculate score data for all funds for all dates.

---
+++ Install/0.3.2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+#  0.3.2.sh
+#  Sam Matthews
+#  23rd February 2019
+#
+#  Install 0.3.2 scripts
+
+SQL_HOME="$HOME/Code/funds/postgres/sql"
+TABS_HOME="$HOME/Code/funds/postgres/tabs"
+FUN_HOME="$HOME/Code/funds/postgres/fun"
+LOA_HOME="$HOME/Code/funds/postgres/load"
+
+# Create tables
+psql -f ${TABS_HOME}/score_data.tab
+
+
+# Create functions
+psql -f ${FUN_HOME}/study_score.fun
